a98f363fc6dda714d3432fdc9acec0099a60a89d
Patrick Woods <patrickjwoods@gmail.com>
2015-04-12 17:43:36 -0500

    update layouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404204103) do
+ActiveRecord::Schema.define(version: 20150412005553) do
 
   create_table "boards", force: true do |t|
     t.string   "title"
@@ -59,9 +59,11 @@ ActiveRecord::Schema.define(version: 20150404204103) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
