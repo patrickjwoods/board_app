a044a877ad09313901d33a142744ac72847bebe1
Patrick Woods <patrickjwoods@gmail.com>
2015-04-16 23:17:49 -0500

    float delete icon right

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,15 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-//= require_tree .
 //= require jquery-ui
+//= require dropzone
+
 
 //= require masonry/jquery.masonry
 //= require masonry/jquery.event-drag
 //= require masonry/jquery.imagesloaded.min
 //= require masonry/jquery.infinitescroll.min
 //= require masonry/modernizr-transitions
+
+//= require_tree .
+
